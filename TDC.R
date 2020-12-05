74d22957e1e52dc396c964206fe24fa09b532bbc
Cesar <jc_pastrana@hotmail.com>
2020-12-04 20:32:30 -0600

    Calculo de datos estadisticos como Media, Mediana, Varianza, Desviacion Estandard y distancia intercuartil

--- TDC.R
+++ TDC.R
@@ -1,4 +1,10 @@
 TDC<-read.table("TDC.txt", header = T)
 par(bg="grey98")
-hist(TDC[,1], col = "lightcyan", main = "Histograma de tipo de cambio", xlab = "Variacion", ylab = "Frecuencia")
+hist(TDC[,1], col = "light blue", main = "Histograma de tipo de cambio", xlab = "Variacion", ylab = "Frecuencia")
 lines(density(TDC[,1]), col="red", lwd=1)
+mean(TDC[,1]) # Calculo de la Media
+median(TDC[,1]) #Calculo de la Mediana
+var(TDC[,1]) # Varianza
+sd(TDC[,1]) #Desviacion estandard
+IQR(TDC[,1]) # Calculo de la distancia interquartil
+
